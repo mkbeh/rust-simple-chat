d30654e7b11297ea92dc4915d7864c115d9844d9
mkbeh <gsilents226@gmail.com>
2025-02-19 19:06:38 +0300

    feat: add cors

--- src/api/router.rs
+++ src/api/router.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
+use axum::http::{HeaderValue, Method};
 use axum::routing::{get, post};
-use axum::Extension;
 use axum::Router;
+use axum::{http, Extension};
 use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
 
 use crate::api;
 use crate::api::Handler;
@@ -16,6 +18,20 @@ pub fn get_router(handler: Arc<Handler>) -> Router {
             .route("/login", post(api::v1::login))
             .route("/messages", post(api::v1::post_message_handler))
             .route("/messages", get(api::v1::list_messages_handler))
+            .layer(
+                CorsLayer::new()
+                    .allow_origin("*".parse::<HeaderValue>().unwrap())
+                    .allow_methods([
+                        Method::GET,
+                        Method::POST,
+                        Method::DELETE,
+                        Method::OPTIONS,
+                        Method::PUT,
+                        Method::HEAD,
+                        Method::PATCH,
+                    ])
+                    .allow_headers([http::header::CONTENT_TYPE]),
+            )
             .layer(ServiceBuilder::new().layer(Extension(handler))),
     );
 
