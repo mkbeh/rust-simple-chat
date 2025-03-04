6492b22a82630a278a9abaaccff7e5c0d63687b0
mkbeh <gsilents226@gmail.com>
2025-03-05 01:32:05 +0300

    fix(swagger): minor fixes

--- src/core_utils/swagger.rs
+++ src/core_utils/swagger.rs
@@ -9,34 +9,34 @@ use utoipa_redoc::{Redoc, Servable};
 use utoipa_scalar::{Scalar, Servable as ScalarServable};
 use utoipa_swagger_ui::SwaggerUi;
 
-pub fn get_openapi_router(router: OpenApiRouter) -> Router {
-    #[derive(OpenApi)]
-    #[openapi(
-        modifiers(&SecurityAddon),
-        servers(
+#[derive(OpenApi)]
+#[openapi(
+    modifiers(&SecurityAddon),
+    servers(
             (url = "http://localhost:9000", description = "local")
-        )
-    )]
-    struct ApiDoc;
+    )
+)]
+struct ApiDoc;
 
-    pub struct SecurityAddon;
+struct SecurityAddon;
 
-    impl Modify for SecurityAddon {
-        fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
-            if let Some(components) = openapi.components.as_mut() {
-                components.add_security_scheme(
-                    "token",
-                    SecurityScheme::Http(
-                        HttpBuilder::new()
-                            .scheme(HttpAuthScheme::Bearer)
-                            .bearer_format("JWT")
-                            .build(),
-                    ),
-                )
-            }
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "token",
+                SecurityScheme::Http(
+                    HttpBuilder::new()
+                        .scheme(HttpAuthScheme::Bearer)
+                        .bearer_format("JWT")
+                        .build(),
+                ),
+            )
         }
     }
+}
 
+pub fn get_openapi_router(router: OpenApiRouter) -> Router {
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .merge(router)
         .split_for_parts();
