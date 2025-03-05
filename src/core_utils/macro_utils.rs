eef39212cc3a0dbc9d51551e3f582914fcf57665
mkbeh <gsilents226@gmail.com>
2025-03-05 20:01:20 +0300

    docs: add macro_utils docs

--- src/core_utils/macro_utils.rs
+++ src/core_utils/macro_utils.rs
@@ -1,3 +1,14 @@
+/// Appends additional method to struct implementation with the same name as param.
+///
+/// # Examples
+///
+/// ```rust
+/// struct Foo;
+///
+/// impl Foo {
+///     crate::self_method!(name, String);
+/// }
+/// ```
 #[macro_export]
 macro_rules! self_method {
     ($name:ident, $ty:ty) => {
